386961c58a0f23a7f47341fb743e019ce29e4341
Kass <kasscrypto@gmail.com>
2021-03-17 20:13:49 +0000

    changed from abi/gen to typechain

--- index.js
+++ index.js
@@ -19,11 +19,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.uniswapV2Router = exports.pancakeMasterChef = exports.erc20 = void 0;
-const erc20 = __importStar(require("./ts/erc20"));
-exports.erc20 = erc20;
-const pancakeMasterChef = __importStar(require("./ts/pancake_master_chef"));
-exports.pancakeMasterChef = pancakeMasterChef;
-const uniswapV2Router = __importStar(require("./ts/uniswap_v2_router"));
-exports.uniswapV2Router = uniswapV2Router;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O0FBQUEsa0RBQW9DO0FBS2hDLHNCQUFLO0FBSlQsNEVBQThEO0FBSzFELDhDQUFpQjtBQUpyQix3RUFBMEQ7QUFLdEQsMENBQWUifQ==
+const all = __importStar(require("./ts"));
+exports.default = all;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7QUFBQSwwQ0FBMkI7QUFDM0Isa0JBQWUsR0FBRyxDQUFDIn0=
---
+++ ts/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UniswapV2Router__factory = exports.PancakeMasterChef__factory = exports.ERC20__factory = void 0;
+var ERC20__factory_1 = require("./factories/ERC20__factory");
+Object.defineProperty(exports, "ERC20__factory", { enumerable: true, get: function () { return ERC20__factory_1.ERC20__factory; } });
+var PancakeMasterChef__factory_1 = require("./factories/PancakeMasterChef__factory");
+Object.defineProperty(exports, "PancakeMasterChef__factory", { enumerable: true, get: function () { return PancakeMasterChef__factory_1.PancakeMasterChef__factory; } });
+var UniswapV2Router__factory_1 = require("./factories/UniswapV2Router__factory");
+Object.defineProperty(exports, "UniswapV2Router__factory", { enumerable: true, get: function () { return UniswapV2Router__factory_1.UniswapV2Router__factory; } });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFPQSw2REFBNEQ7QUFBbkQsZ0hBQUEsY0FBYyxPQUFBO0FBQ3ZCLHFGQUFvRjtBQUEzRSx3SUFBQSwwQkFBMEIsT0FBQTtBQUNuQyxpRkFBZ0Y7QUFBdkUsb0lBQUEsd0JBQXdCLE9BQUEifQ==
