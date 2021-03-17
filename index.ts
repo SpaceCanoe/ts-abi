9df580b6133c64c5fc80c437173beaff318d5bf8
Kass <kasscrypto@gmail.com>
2021-03-17 18:43:27 +0000

    added index file

---
+++ index.ts
@@ -0,0 +1,9 @@
+import * as erc20 from "./ts/erc20";
+import * as pancakeMasterChef from "./ts/pancake_master_chef";
+import * as uniswapV2Router from "./ts/uniswap_v2_router";
+
+export {
+    erc20,
+    pancakeMasterChef,
+    uniswapV2Router,
+}
